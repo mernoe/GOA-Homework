b3f7934ee356e2d34f7ba49df2f0318c45b0d6dc
mernoe <schizogeneral@gmail.com>
2024-05-19 17:43:08 +0400

    second commit

---
+++ level 3/Homework/level3.py
@@ -0,0 +1,17 @@
+int1 = 50
+float1 = 50.0
+
+print(int1 + float1)
+#The output is present like this due to mathematics
+
+bool1 = True
+bool2 = False
+
+print(int(bool1 + bool2))
+#True equals 1 False equals 0 and if we add the two it will be 1+0 which willhave an ouput of 1.
+
+
+int3 = 20
+int4 = 24
+
+print(str(int3) + str(int4))
